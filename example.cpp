45e0fee91cef3cd39267e98852080b33c204f6b9
Khaley-dev <devkhaley@gmail.com>
2026-07-17 19:11:56 +0000

    creation of structure of NN

---
+++ example.cpp
@@ -0,0 +1,5 @@
+#include "funn.hpp"
+
+int main(){
+    funn::NN<>({1,10,1});
+}
--- funn.hpp
+++ funn.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include <vector>
 #include <concepts>
+#include <cmath>
+#include <memory>
+#include <random>
 
 namespace funn{
 
@@ -24,17 +27,67 @@ num ReLU(num x){
     return (x>num(0))?x:num(0);
 }
 
+// example random initialization function for weights
+
+template<Number num = double>
+num RandInitWeights(){
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_real_distribution<double>dist(-1.0,1.0);
+    return num(dist(gen));
+}
+
 template<Number num = double>
 struct NN{
     struct Neuron{
         num value;
         num bias;
-        Neuron(num v = num{}, num b = num{}) : value(v), bias(b) {}
+        std::vector<num>weights;
+        Neuron(
+            size_t wsize,
+            num(*initFunc)(),
+            num v,
+            num b)
+        : value(v), bias(b){
+            weights.resize(wsize);
+            for(auto& i:weights) i = initFunc();
+        }
     };
     
     struct Layer{
+        std::vector<Neuron>neurons;
+        size_t lsize;
+        size_t ins,outs{};
+
+        num(*activationFunction)(num);
+        num(*neuronInitFunction)();
         
+        Layer(
+            size_t lsize,
+            size_t in,
+            num(*activFunc)(num) = ReLU<num>,
+            num(*initFunc)() = RandInitWeights<num>,
+            num defaultNeuronBias = num{},
+            num defaultNeuronValue = num{}
+        ) : lsize(lsize), ins(in),
+        activationFunction(activFunc),
+        neuronInitFunction(initFunc) {
+            neurons.assign(lsize,Neuron(in, initFunc, defaultNeuronValue, defaultNeuronBias));
+        }
     };
+
+    std::vector<Layer>layers;
+
+    NN(
+        std::vector<size_t>layer_sizes
+    ){
+        for(size_t i{};i<layer_sizes.size();i++){
+            layers.emplace_back(Layer(
+                layer_sizes[i],
+                (i==0) ? 0 : layer_sizes[i-1] 
+            ));
+        }
+    }
 };
 
 
