e0a3b2d3b53ced2f1fa905168e4120b759d04af2
Khaley-dev <devkhaley@gmail.com>
2026-07-17 13:45:18 +0000

    some

--- funn.hpp
+++ funn.hpp
@@ -1,1 +1,42 @@
 #pragma once
+#include <vector>
+#include <concepts>
+
+namespace funn{
+
+template <typename T>
+concept Number =
+    std::copyable<T> &&
+    std::totally_ordered<T> &&
+    requires(T a, T b) {
+        {a+b} -> std::convertible_to<T>;
+        {a-b} -> std::convertible_to<T>;
+        {a*b} -> std::convertible_to<T>;
+        {a/b} -> std::convertible_to<T>;
+        {a+=b} -> std::convertible_to<T&>;
+        {a-=b} -> std::convertible_to<T&>;
+        {a*=b} -> std::convertible_to<T&>;
+        {a/=b} -> std::convertible_to<T&>;
+    };
+
+template<Number num = double>
+num ReLU(num x){
+    return (x>num(0))?x:num(0);
+}
+
+template<Number num = double>
+struct NN{
+    struct Neuron{
+        num value;
+        num bias;
+        Neuron(num v = num{}, num b = num{}) : value(v), bias(b) {}
+    };
+    
+    struct Layer{
+        
+    };
+};
+
+
+
+}
