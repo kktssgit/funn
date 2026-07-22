66876a7e45cb6d11cdb397788e4977469c05099c
Khaley-dev <devkhaley@gmail.com>
2026-07-23 01:08:27 +0200

    added learning, big commit

--- example.cpp
+++ example.cpp
@@ -1,17 +1,27 @@
 #include "funn.hpp"
 
+#include <iomanip>
+#include <iostream>
+
 int main(){
+
+/*
     funn::NN<> nn({1,3,1});
     std::cout<<"layer 1: \n"; nn.layers[0].printNeurons();
     std::cout<<"layer 2: \n"; nn.layers[1].printNeurons();
     std::cout<<"layer 3: \n"; nn.layers[2].printNeurons();
 
-    funn::Data data{{5.0},3.0};
-    auto out = nn.calc(data);
+    funn::Data data{{5.0},{3.0}};
+    nn.calc(data);
+    auto out = nn.getOutput();
 
     std::cout<<out.output.front()<<'\n';
 
     std::cout<<"layer 1: \n"; nn.layers[0].printNeurons();
     std::cout<<"layer 2: \n"; nn.layers[1].printNeurons();
     std::cout<<"layer 3: \n"; nn.layers[2].printNeurons();
+
+*/
+
+
 }
--- funn.hpp
+++ funn.hpp
@@ -11,6 +11,8 @@
 
 namespace funn{
 
+// concept for dynamic typing
+
 template <typename T>
 concept Number =
     std::copyable<T> &&
@@ -26,12 +28,24 @@ concept Number =
         {a/=b} -> std::convertible_to<T&>;
     };
 
+
+
+// example functions
+
 template<Number num = double>
 num ReLU(num x){
-    return (x>num(0))?x:num(0);
+    return x>0?x:0;
 }
 
-// example random initialization function for weights
+template<Number num = double>
+num ReLU_derivative(num x){
+    return x>0?1:0;
+}
+
+template<Number num = double>
+num derive(num(*fun)(num), num x, num h = num(0.000001)){
+    return (fun(x+h)-fun(x))/h;
+}
 
 template<Number num = double>
 num RandInitWeights(){
@@ -42,9 +56,21 @@ num RandInitWeights(){
 }
 
 template<Number num = double>
+num diffSquared(num predicted, num expected){
+    return (0.5)*(predicted-expected)*(predicted-expected);
+}
+
+template<Number num = double>
+num diffSquared_dx(num predicted, num expected){
+    return (predicted-expected);
+}
+
+// data structures
+
+template<Number num = double>
 struct Data{
     std::vector<num>input;
-    num expected;
+    std::vector<num>expected;
 };
 
 template<Number num = double>
@@ -56,8 +82,15 @@ template<Number num = double>
 struct NN{
     struct Neuron{
         num value;
+        num value_pa{}; // value pre activation function
         num bias;
         std::vector<num>weights;
+
+        std::vector<num>weight_gradients={};
+        num bias_gradient={};
+
+        num delta{};
+
         Neuron(
             size_t wsize,
             num(*initFunc)(),
@@ -65,6 +98,7 @@ struct NN{
             num b)
         : value(v), bias(b){
             weights.resize(wsize);
+            weight_gradients.assign(wsize,num(0.0));
             for(auto& i:weights) i = initFunc();
         }
     };
@@ -75,18 +109,21 @@ struct NN{
         size_t ins,outs{};
 
         num(*activationFunction)(num);
-        num(*neuronInitFunction)();
+        num(*activationFunction_dx)(num); // activation function derivative
+        // num(*neuronInitFunction)();
         
         Layer(
             size_t lsize,
             size_t in,
             num(*activFunc)(num) = ReLU<num>,
+            num(*activFuncDx)(num) = ReLU_derivative<num>,
             num(*initFunc)() = RandInitWeights<num>,
             num defaultNeuronBias = num{},
             num defaultNeuronValue = num{}
         ) : lsize(lsize), ins(in),
         activationFunction(activFunc),
-        neuronInitFunction(initFunc) {
+        activationFunction_dx(activFuncDx)
+        /* neuronInitFunction(initFunc) */ {
             for(size_t i{};i<lsize;i++)
                 neurons.emplace_back(Neuron(in, initFunc, defaultNeuronValue, defaultNeuronBias));
         }
@@ -106,15 +143,23 @@ struct NN{
     size_t input_size{};
     size_t output_size{};
     std::vector<Layer>layers;
+    std::vector<num>loss;
+
+    num(*lossFunction)(num,num);
+    num(*lossFunction_dx)(num,num);
 
     // make another constructor which accepts all the other params
 
     NN( // will fail if layer_sizes is empty i think
-        const std::vector<size_t>&layer_sizes
+        const std::vector<size_t>&layer_sizes,
+        num(*lossFun)(num,num) = diffSquared<>,
+        num(*lossFunDx)(num,num) = diffSquared_dx<>
     ) : 
         input_size(layer_sizes.front()),
         output_size(layer_sizes.back()),
-        layers_amount(layer_sizes.size())
+        layers_amount(layer_sizes.size()),
+        lossFunction(lossFun),
+        lossFunction_dx(lossFunDx)
     {
         for(size_t i{};i<layer_sizes.size();i++){
             layers.emplace_back(Layer(
@@ -122,36 +167,101 @@ struct NN{
                 (i==0) ? 0 : layer_sizes[i-1] 
             ));
         }
+
+        loss.assign(output_size,num(0.0));
     }
 
-    Output<num> calc(const Data<num>& data){
+    void calc(const Data<num>&data, bool learning = false){
         auto& in = data.input;
 
         assert(in.size() == input_size);
 
-        Output out{std::vector<num>()};
-
         for(auto layer_p = layers.begin(); layer_p != layers.end(); layer_p++){
             if(layer_p == layers.begin()){
                 size_t n_i{};
-                for(auto& neuron : layer_p->neurons) neuron.value = in[n_i++];
+                for(auto& neuron : layer_p->neurons) neuron.value_pa = neuron.value = in[n_i++];
                 continue;
             }
 
             auto prev_layer = layer_p; prev_layer--;
 
             for(auto& neuron : layer_p->neurons){
-                neuron.value = neuron.bias;
+                neuron.value_pa = neuron.bias;
                 for(size_t i{};i<prev_layer->neurons.size();i++)
-                    neuron.value += prev_layer->neurons[i].value * neuron.weights[i];
-                neuron.value = layer_p->activationFunction(neuron.value);
+                    neuron.value_pa += prev_layer->neurons[i].value * neuron.weights[i];
+                neuron.value = layer_p->activationFunction(neuron.value_pa);
             }
         }
 
-        for(const auto& neuron : layers.back().neurons) out.output.push_back(neuron.value);
+        if(learning)
+            for(size_t l_i{};l_i<layers.back().neurons.size();l_i++)
+                loss[l_i]=lossFunction(layers.back().neurons[l_i].value,data.expected[l_i]);
+    }
 
+    Output<num> getOutput(){
+        Output out{std::vector<num>()};
+        for(const auto& neuron : layers.back().neurons) out.output.push_back(neuron.value);
         return out;
     }
+
+    void learn(const std::vector<Data<num>>&dataset, double learning_rate = 0.01){
+        // clear all gradients
+        for(auto& layer:layers)
+            for(auto& neuron:layer.neurons)
+                std::fill(neuron.weight_gradients.begin(),neuron.weight_gradients.end(),num(0.0)), 
+                neuron.bias_gradient = num(0.0);
+
+        for(auto& dataPoint:dataset){
+            calc(dataPoint, 1);
+
+            for(auto layer_p = layers.rbegin(); layer_p != layers.rend()-1; layer_p++){
+                
+                auto prev_layer = layer_p+1;
+
+                if(layer_p == layers.rbegin()){
+                    size_t d_i{};
+                    for(auto& neuron:layer_p->neurons){
+                        neuron.delta = lossFunction_dx(neuron.value, dataPoint.expected[d_i++]) *
+                                       layer_p->activationFunction_dx(neuron.value_pa);
+
+                        neuron.bias_gradient += neuron.delta;
+
+                        for(size_t w_i{}; w_i<neuron.weights.size();w_i++){
+                            neuron.weight_gradients[w_i] += neuron.delta * prev_layer->neurons[w_i].value;
+                        }
+                    }
+                } else {
+                    auto next_layer = layer_p-1;
+
+                    for(auto& neuron:layer_p->neurons){
+                        size_t k_i{};
+                        
+                        num error_sum = num(0.0);
+                        for(auto& next_neuron:next_layer->neurons){
+                            error_sum += next_neuron.delta * next_neuron.weights[k_i];
+                        }
+                        neuron.delta = error_sum * layer_p->activationFunction_dx(neuron.value_pa);
+
+                        neuron.bias_gradient += neuron.delta;
+
+                        for(size_t w_i{}; w_i < neuron.weights.size();w_i++)
+                            neuron.weight_gradients[w_i] += neuron.delta * prev_layer->neurons[w_i].value;
+
+                        k_i++;
+                    }
+                }
+            }
+        }
+
+        for(auto& layer:layers){
+            for(auto& neuron:layer.neurons){
+                for(size_t w_i{}; w_i<neuron.weights.size();w_i++)
+                    neuron.weights[w_i] -= (neuron.weight_gradients[w_i]/dataset.size())*learning_rate;
+                neuron.bias -= (neuron.bias_gradient/dataset.size())*learning_rate;
+            }
+        }
+    }
+
 };
 
 
